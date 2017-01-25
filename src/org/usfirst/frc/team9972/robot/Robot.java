8144c273d65007927b7ef1a6413ef150a6da747b
Justin Quan <Justin Quan>
2017-01-24 22:57:23 -0800

    update

--- src/org/usfirst/frc/team9972/robot/Robot.java
+++ src/org/usfirst/frc/team9972/robot/Robot.java
@@ -5,12 +5,8 @@ import com.analog.adis16448.frc.ADIS16448_IMU;
 import edu.wpi.first.wpilibj.*;
 
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.interfaces.Accelerometer;
-import edu.wpi.first.wpilibj.interfaces.Accelerometer.Range;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
-import edu.wpi.first.wpilibj.ADXL362;
 
 public class Robot extends IterativeRobot {
 	ADIS16448_IMU imu = new ADIS16448_IMU();
@@ -24,20 +20,9 @@ public class Robot extends IterativeRobot {
 	Victor frontRightMotor = new Victor(2);
 	Victor frontLeftMotor = new Victor(3);
 	
-	Accelerometer.Range range;
-	ADXL362 accel = new ADXL362(Range.k4G);
-	ADXRS450_Gyro gyro = new ADXRS450_Gyro();
-	
-//	Victor pidStraightValue = new Victor(10); //this is sketchy code, shouldn't actually run any motors
-//	PIDController pidDriveStraight = new PIDController(0, 0, 0, gyro, pidStraightValue);
-//	
-//	Victor pidTurnValue = new Victor(11); //this is sketchy code, shouldn't actually run any motors //crashes program, robot's don't quit error
-//	PIDController pidTurn = new PIDController(0, 0, 0, gyro, pidTurnValue);// robots don't quit
-	
 	double setAngle_Straight = 0.0;
 	double setAngle_Turn = 0.0;
-	
-	//we also need to correct the angle output so that -360 = +360 and +360 = 0 but don't worry about it now
+	double turnSpeed = .5;
 	
 	double driveStraightSpeed = 0.0;
 
@@ -48,14 +33,22 @@ public class Robot extends IterativeRobot {
 	boolean leftPressedLastTime = false;
 	boolean driveStraight = false;
 	
-	boolean rightPressedLastTime = false;
-	boolean turnToPoint = false;
+	boolean right1PressedLastTime = false;
+	boolean right2PressedLastTime = false;
+	boolean right3PressedLastTime = false;
+
+	boolean right1IsPressed = false;
+	boolean right2IsPressed = false;
+	boolean right3IsPressed = false;
 	
-
-
+	boolean tankDrive = true;
+	double initAngleX, initAngleY, initAngleZ;
+	
 	public void teleopInit() {
-		gyro.calibrate();
 		imu.calibrate();
+		initAngleX = imu.getAngleX();
+		initAngleY = imu.getAngleY();
+		initAngleZ = imu.getAngleZ();
 		/*
 		 * Graph gets all messed up if we move our robot before the graph calibrates.
 		 * We need to wait approx 3 seconds (not positive; it would be best to remeasure)
@@ -63,10 +56,7 @@ public class Robot extends IterativeRobot {
 		*/
 		
 	}
-
 	public void autonomousPeriodic() {
-		while((imu.getAngleX()/4)<180){
-		  drive.tankDrive(-.5, .5);
 		  SmartDashboard.putData("IMU", imu); 
 		  SmartDashboard.putNumber("Angle Y", imu.getAngleY()/4);
 		  SmartDashboard.putNumber("Angle X", imu.getAngleX()/4);
@@ -75,80 +65,60 @@ public class Robot extends IterativeRobot {
 		  SmartDashboard.putNumber("Accel Y", imu.getAccelY());
 		  SmartDashboard.putNumber("Accel Z", imu.getAccelZ());
 		}
-	}
 	
 	public void teleopPeriodic() {
-		/*
-		double currentAngle = gyro.getAngle();
-		boolean leftIsPressed = leftJoy.getRawButton(1);
 		
-		double kP = (((leftJoy.getZ() * -1) + 1) / 2.0) * 0.03;
-		double kI = (((rightJoy.getZ() * -1) + 1) / 2.0) * 0.0008;
-		double kD = (((operatorJoy.getThrottle() * -1) + 1) / 2.0) * 0.005;
-
-		if (leftIsPressed && !leftPressedLastTime) {
-			driveStraight = !driveStraight;
-			setAngle_Straight = currentAngle;
-			if(driveStraight) {
-				pidDriveStraight.setPID(-0.018, -0.0008, 0.0002); //D is in the opposing direction of P and I
-				pidDriveStraight.setSetpoint(setAngle_Straight);
-				pidDriveStraight.enable();
-			} else {
-				pidDriveStraight.reset();
-			}
-		}
-		leftPressedLastTime = leftIsPressed;
-		
-
-		boolean rightIsPressed = rightJoy.getRawButton(1);
-		if (rightIsPressed && !rightPressedLastTime) {
-			turnToPoint = !turnToPoint;
-			setAngle_Turn = currentAngle + 90.0;
-			if(turnToPoint) {
-				pidTurn.setPID(-kP, -kI, kD);
-				pidTurn.setSetpoint(setAngle_Turn);
-				pidTurn.enable();
-			} else {
-				pidTurn.reset();
-			}
-		}
-		rightPressedLastTime = rightIsPressed;
-		
-		if (driveStraight) {
-			driveStraightSpeed = -leftJoy.getY(); //joysticks wrong sign
-			drive.tankDrive(driveStraightSpeed - (1 - Math.abs(driveStraightSpeed)) * pidDriveStraight.get(), driveStraightSpeed + (1 - Math.abs(driveStraightSpeed)) * pidDriveStraight.get()); //this code should get the output of the pid and add it to the desired speed so that the robot moves at desired speed while using pid to adjust the angle (and never making speed values over +/-1)
-		} else if (turnToPoint) {
-			drive.tankDrive(-pidTurn.get(), pidTurn.get());
-		} else {
-			drive.tankDrive(-leftJoy.getY(), -rightJoy.getY()); //joysticks wrong sign
-		}
-
-		acceleration[0] = accel.getX();
-		acceleration[1] = accel.getY();
-		acceleration[2] = accel.getZ();
-		SmartDashboard.putBoolean("driveStraight", driveStraight);
-		SmartDashboard.putBoolean("turnToPoint", turnToPoint);
+	    acceleration[0] = imu.getAccelX();// IMU not tested yet with acceleration
+		acceleration[1] = imu.getAccelY();
+		acceleration[2] = imu.getAccelZ();
+		SmartDashboard.putData("IMU", imu); 		
 		SmartDashboard.putNumber("X Acceleration", acceleration[0]);
 		SmartDashboard.putNumber("Y Acceleration", acceleration[1]);
 		SmartDashboard.putNumber("Z Acceleration", acceleration[2]);
-		SmartDashboard.putNumber("Angle Graph", currentAngle % 360);
-		SmartDashboard.putNumber("Error Turn", -setAngle_Turn + currentAngle);
-		SmartDashboard.putNumber("Current Angle Value", currentAngle); //to have both graph and value displayed simultaneously on Smart Dashboard
-		SmartDashboard.putNumber("P * 10", kP * 10);
-		SmartDashboard.putNumber("I * 100", kI*100);
-		SmartDashboard.putNumber("D * 10", kD*10);*/
+		SmartDashboard.putNumber("Angle X", imu.getAngleX()/4);//For all angles, scale by dividing by 4 (ex: a 90 degree turn by the robot returns a Z angle change of 360)
+		SmartDashboard.putNumber("Angle Y", imu.getAngleY()/4);
+		SmartDashboard.putNumber("Angle Z", imu.getAngleZ()/4);
 		
-		
-		while((imu.getAngleZ()/4)< 180){//need to make it so that it does 180 turn after a button price
-			// need to save last angle and make it so that it turns until it becomes either Z initial position / 4 before +180 or -180
-			  drive.tankDrive(.5, -.5);
-			  SmartDashboard.putData("IMU", imu); 
-			  SmartDashboard.putNumber("Angle Y", imu.getAngleY()/4);//For all angles, scale by dividing by 4 (ex: a 90 degree turn by the robot returns a Z angle change of 360)
-			  SmartDashboard.putNumber("Angle X", imu.getAngleX()/4);
-			  SmartDashboard.putNumber("Angle Z", imu.getAngleZ()/4);
-			  SmartDashboard.putNumber("Accel X", imu.getAccelX());
-			  SmartDashboard.putNumber("Accel Y", imu.getAccelY());
-			  SmartDashboard.putNumber("Accel Z", imu.getAccelZ());
+		right1IsPressed = rightJoy.getRawButton(1);
+		if (right1IsPressed && !right1PressedLastTime) {
+			tankDrive = !tankDrive;
 		}
+		right1PressedLastTime = right1IsPressed;
+
+		
+		if (tankDrive){
+			drive.tankDrive(-leftJoy.getY(), -rightJoy.getY()); //joysticks wrong sign
+		}
+
+		right2IsPressed = rightJoy.getRawButton(2);
+		if (right2IsPressed && !right2PressedLastTime) {
+			double lastAngleZ = imu.getAngleZ()/4;
+			while((imu.getAngleZ()/4) < lastAngleZ + 180){
+				  drive.tankDrive(turnSpeed, -turnSpeed);
+				  SmartDashboard.putData("IMU", imu); 
+				  SmartDashboard.putNumber("Angle Y", imu.getAngleY()/4);//For all angles, scale by dividing by 4 (ex: a 90 degree turn by the robot returns a Z angle change of 360)
+				  SmartDashboard.putNumber("Angle X", imu.getAngleX()/4);
+				  SmartDashboard.putNumber("Angle Z", imu.getAngleZ()/4);
+				  SmartDashboard.putNumber("Acceleration X", imu.getAccelX());
+				  SmartDashboard.putNumber("Acceleration Y", imu.getAccelY());
+				  SmartDashboard.putNumber("Acceleration Z", imu.getAccelZ());
+				  SmartDashboard.putNumber("last angle z", lastAngleZ);
+				  
+			}
+		}
+		right2PressedLastTime = right2IsPressed;
+	/*	right3IsPressed = rightJoy.getRawButton(3);//doesn't work yet
+		if (right3IsPressed && !right3PressedLastTime) {//should return robot to original Z front facing position from teleOpInit
+			double rangedAngleZ = imu.getAngleZ()/4 % 360.0;//ranged angle is an angle within the range of -360 to 360
+			while(rangedAngleZ != initAngleZ){
+				if(rangedAngleZ < initAngleZ){
+					drive.tankDrive(turnSpeed, -turnSpeed);
+				}
+				else if(rangedAngleZ > initAngleZ){
+					drive.tankDrive(-turnSpeed, turnSpeed);
+				}
+			}
+		}
+		right3PressedLastTime = right3IsPressed;*/
 	}
 }
